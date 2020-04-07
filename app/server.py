8edb9e6ebb5b4a0edd71d0150acc82dbd6266fb4
old0nick <old0nick@mail.ru>
2020-04-08 00:38:54 +0300

    My Homework

--- app/server.py
+++ app/server.py
@@ -16,18 +16,34 @@ class ServerProtocol(asyncio.Protocol):
     def data_received(self, data: bytes):
         print(data)
 
-        decoded = data.decode()
+        decoded = data.decode().replace("\r\n", "")
 
         if self.login is not None:
-            self.send_message(decoded)
+            if decoded != "":
+                if decoded == "quit":
+                    self.transport.close()
+                else:
+                    self.send_message(False, decoded)
         else:
             if decoded.startswith("login:"):
-                self.login = decoded.replace("login:", "").replace("\r\n", "")
-                self.transport.write(
-                    f"Привет, {self.login}!\n".encode()
-                )
+                newlogin = decoded.replace("login:", "").replace("\r\n", "")
+                login_busy = False
+                for client in self.server.clients:
+                    if not login_busy:
+                        login_busy = newlogin == client.login
+                if login_busy:
+                    self.transport.write(f"Логин {newlogin} занят, попробуйте другой".encode())
+                    self.transport.close()
+                else:
+                    self.login = newlogin
+                    self.transport.write(f"Привет, {self.login}!\r\n".encode())
+                    if len(self.server.history) > 0:
+                        self.transport.write(f"Последние сообщения:\r\n".encode())
+                        for message in self.server.history:
+                            self.transport.write(f"{message}".encode())
+                    self.send_message(True, f"{self.login} подключился.")
             else:
-                self.transport.write("Неправильный логин\n".encode())
+                self.transport.write("Неправильный логин\r\n".encode())
 
     def connection_made(self, transport: transports.Transport):
         self.server.clients.append(self)
@@ -37,19 +53,27 @@ class ServerProtocol(asyncio.Protocol):
     def connection_lost(self, exception):
         self.server.clients.remove(self)
         print("Клиент вышел")
+        if self.login is not None:
+            self.send_message(True, f"{self.login} отключился.")
 
-    def send_message(self, content: str):
-        message = f"{self.login}: {content}\n"
-
+    def send_message(self, info: bool, content: str):
+        message = f"{content}\r\n"
+        if not info:
+            message = f"{self.login}: " + message
+        self.server.history.append(message)
+        if len(self.server.history) > 10:
+            del(self.server.history[0])
         for user in self.server.clients:
-            user.transport.write(message.encode())
-
+            if self.login != user.login:
+                user.transport.write(message.encode())
 
 class Server:
     clients: list
+    history: list
 
     def __init__(self):
         self.clients = []
+        self.history = []
 
     def build_protocol(self):
         return ServerProtocol(self)
