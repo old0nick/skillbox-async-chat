e8a56cd93e7152459fb676ae962ceb9500258b77
old0nick <old0nick@mail.ru>
2020-04-08 22:24:38 +0300

    My Homework

--- src/day_03/00_handlers.py
+++ src/day_03/00_handlers.py
@@ -1,3 +1,17 @@
 """
 Пример программы для работы с интерфейсами Qt
 """
+from PySide2.QtWidgets import QMainWindow, QApplication
+
+from app.interface import Ui_MainWindow
+
+class MainWindow(QMainWindow, Ui_MainWindow)
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+app = QApplication()
+window = MainWindow()
+
+window.show()
+app.exec_()
